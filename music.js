20137b37e287fcdab1b4138ed596e9f8805271dd
Anvarkz <140018516+Anvarkz@users.noreply.github.com>
2024-02-13 01:56:44 +0600

    Update music.js

--- music.js
+++ music.js
@@ -55,7 +55,7 @@ $(function () {
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10"],
     trackUrl = [
-      "https://mp3uk.net/mp3/files/ricchi-poveri-sara-perche-ti-amo-mp3.mp3",
+      "https://eu.hitmotop.com/get/music/20170919/Ricchi_E_Poveri_-_Sar_perch_ti_amo_48689644.mp3",
       "https://eu.hitmotop.com/get/music/20170902/Joe_Dassin_-_Et_si_tu_nexistais_pas_48004460.mp3",
       "https://eu.hitmotop.com/get/music/20110208/Umberto_Tozzi_feat_Monica_Bellucci_-_Ti_amo_391958.mp3",
       "https://eu.hitmotop.com/get/music/20170903/Andrea_Bocelli_-_Cant_Help_Falling_In_Love_48076869.mp3",
