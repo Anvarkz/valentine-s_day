617b16b28a1770cb3210f1b6956e0c74c41c7c2f
Anvarkz <140018516+Anvarkz@users.noreply.github.com>
2024-02-13 01:45:59 +0600

    Update music.js

--- music.js
+++ music.js
@@ -55,7 +55,7 @@ $(function () {
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10"],
     trackUrl = [
-      "https://eu.hitmotop.com/get/music/20170919/Ricchi_E_Poveri_-_Sar_perch_ti_amo_48689644.mp3",
+      "https://cdn.muzsky.net/?h=JGraYpdVSDcUt6CCvgg14LA9mc6qckiqMMvgpywL5pFIXjSXS0R0BezBrEC-TC0IzSZyn61risMYmkN6gBz1lFgzxWCgVQ%5C%5C",
       "https://eu.hitmotop.com/get/music/20170902/Joe_Dassin_-_Et_si_tu_nexistais_pas_48004460.mp3",
       "https://eu.hitmotop.com/get/music/20110208/Umberto_Tozzi_feat_Monica_Bellucci_-_Ti_amo_391958.mp3",
       "https://eu.hitmotop.com/get/music/20170903/Andrea_Bocelli_-_Cant_Help_Falling_In_Love_48076869.mp3",
